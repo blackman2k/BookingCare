1baec59d10f157c871fcdaaa153431c8f481afd5
Tran Huu Canh <tranhuucanh2000@gmail.com>
2021-12-13 22:30:00 +0700

    update chon lich kham

--- BE/src/services/doctorService.js
+++ BE/src/services/doctorService.js
@@ -1,5 +1,9 @@
 import db from "../models"
 import userService from "./userSerivice"
+require("dotenv").config()
+import _, { reject } from "lodash"
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE
 
 let getTopDoctorHome = (limitInput) => {
   return new Promise(async (resolve, reject) => {
@@ -149,9 +153,59 @@ const getDetailDoctorById = (id) => {
   })
 }
 
+const bulkCreateSchedule = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+        resolve({
+          errCode: -1,
+          errMessage: "Missing required parameters!",
+        })
+      } else {
+        let schedule = data.arrSchedule
+        if (schedule && schedule.length > 0) {
+          schedule = schedule.map((item) => {
+            item.maxNumber = MAX_NUMBER_SCHEDULE
+            return item
+          })
+        }
+
+        let existing = await db.Schedule.findAll({
+          where: { doctorId: data.doctorId, date: data.formatedDate },
+          attributes: ["timeType", "date", "doctorId", "maxNumber"],
+          raw: true,
+        })
+
+        if (existing && existing.length > 0) {
+          existing = existing.map((item) => {
+            item.date = new Date(item.date).getTime()
+            return item
+          })
+        }
+
+        let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+          return a.timeType === b.timeType && a.date === b.date
+        })
+
+        if (toCreate && toCreate.length > 0) {
+          await db.Schedule.bulkCreate(toCreate)
+        }
+
+        resolve({
+          errCode: 0,
+          errMessage: "OK",
+        })
+      }
+    } catch (error) {
+      reject(e)
+    }
+  })
+}
+
 module.exports = {
   getTopDoctorHome,
   getAllDoctors,
   saveDetailInforDoctor,
   getDetailDoctorById,
+  bulkCreateSchedule,
 }
