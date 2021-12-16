818a98a0055f4651d556932d397576214ebe4260
Tran Huu Canh <tranhuucanh2000@gmail.com>
2021-12-16 13:54:58 +0700

    tao api xac nhan lich kham

--- BE/src/controllers/patientController.js
+++ BE/src/controllers/patientController.js
@@ -12,7 +12,20 @@ const postBookAppointment = async (req, res) => {
     })
   }
 }
+const postVerifyBookAppointment = async (req, res) => {
+  try {
+    let infor = await patientService.postVerifyBookAppointment(req.body)
+    return res.status(200).json(infor)
+  } catch (error) {
+    console.log(error)
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    })
+  }
+}
 
 module.exports = {
   postBookAppointment,
+  postVerifyBookAppointment,
 }
